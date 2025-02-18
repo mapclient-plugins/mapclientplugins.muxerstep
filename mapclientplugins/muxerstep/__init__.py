2ddad70134ac7f4f209566de6ea0bb1a01608449
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-02-18 14:03:57 +1300

    Remove .git from end of __location__.

--- mapclientplugins/muxerstep/__init__.py
+++ mapclientplugins/muxerstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.2.1'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Muxer'
-__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.muxerstep.git'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.muxerstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.muxerstep import step
